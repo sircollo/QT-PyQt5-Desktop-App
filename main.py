cb1266ac0ba32a9525d11bd071aadf62e4269306
sircollo <sircollo71@gmail.com>
2023-01-06 21:54:30 +0300

    Feat: adds net counters, net addressesand net connections

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import time
 import datetime
 import shutil
 from multiprocessing import cpu_count
-
+import platform
 # Global
 platforms = {
 	'linux': 'Linux',
@@ -316,20 +316,20 @@ class MainWindow(QMainWindow):
    
 	# sensors
 	def sensors(self):
-		# if sys.platform == "linux" or sys.platform == "linux1" or sys.platform == "linux2":
-		try:
-			for x in psutil.sensors_temperatures():
-				for y in psutil.sensors_temperatures()[x]:
-					rowPosition = self.ui.sensorTable.rowCount()
-					self.ui.sensorTable.insertRow(rowPosition)
+		if sys.platform == "linux" or sys.platform == "linux1" or sys.platform == "linux2":
+			try:
+				for x in psutil.sensors_temperatures():
+					for y in psutil.sensors_temperatures()[x]:
+						rowPosition = self.ui.sensorTable.rowCount()
+						self.ui.sensorTable.insertRow(rowPosition)
 
-					self.create_table_widget(rowPosition, 0, x, "sensorTable")
-					self.create_table_widget(rowPosition, 1, y.label, "sensorTable")
-					self.create_table_widget(rowPosition, 2, str(y.current), "sensorTable")
-					self.create_table_widget(rowPosition, 3, str(y.high), "sensorTable")
-					self.create_table_widget(rowPosition, 4, str(y.critical), "sensorTable")
-		except Exception as e:
-			print(e)
+						self.create_table_widget(rowPosition, 0, x, "sensorTable")
+						self.create_table_widget(rowPosition, 1, y.label, "sensorTable")
+						self.create_table_widget(rowPosition, 2, str(y.current), "sensorTable")
+						self.create_table_widget(rowPosition, 3, str(y.high), "sensorTable")
+						self.create_table_widget(rowPosition, 4, str(y.critical), "sensorTable")
+			except Exception as e:
+				print(e)
   		# else:
 		# 	global platforms
 		# 	rowPosition = self.ui.sensorTable.rowCount()
@@ -354,6 +354,48 @@ class MainWindow(QMainWindow):
 			self.create_table_widget(rowPosition, 2, str(z[x].duplex), "net_stats_table")
 			self.create_table_widget(rowPosition, 3, str(z[x].speed), "net_stats_table")
 			self.create_table_widget(rowPosition, 4, str(z[x].mtu), "net_stats_table")
+
+		for x in psutil.net_io_counters(pernic=True):
+			z = psutil.net_io_counters(pernic=True)
+			rowPosition = self.ui.net_io_table.rowCount()
+			self.ui.net_io_table.insertRow(rowPosition)
+
+			self.create_table_widget(rowPosition, 0, x, "net_io_table")
+			self.create_table_widget(rowPosition, 1, str(z[x].bytes_sent), "net_io_table")
+			self.create_table_widget(rowPosition, 2, str(z[x].bytes_recv), "net_io_table")
+			self.create_table_widget(rowPosition, 3,str(z[x].packets_sent), "net_io_table")
+			self.create_table_widget(rowPosition, 4, str(z[x].packets_recv), "net_io_table")
+			self.create_table_widget(rowPosition, 5, str(z[x].errin), "net_io_table")
+			self.create_table_widget(rowPosition, 6, str(z[x].errout), "net_io_table")
+			self.create_table_widget(rowPosition, 7, str(z[x].dropin), "net_io_table")
+			self.create_table_widget(rowPosition, 8, str(z[x].dropout), "net_io_table")
+   
+		for x in psutil.net_if_addrs():
+			z = psutil.net_if_addrs()
+			for y in z[x]:
+				rowPosition = self.ui.net_addresses_table.rowCount()
+				self.ui.net_addresses_table.insertRow(rowPosition)
+
+				self.create_table_widget(rowPosition, 0, str(x), "net_addresses_table")
+				self.create_table_widget(rowPosition, 1, str(y.family), "net_addresses_table")
+				self.create_table_widget(rowPosition, 2, str(y.address), "net_addresses_table")
+				self.create_table_widget(rowPosition, 3,str(y.netmask), "net_addresses_table")
+				self.create_table_widget(rowPosition, 4, str(y.broadcast), "net_addresses_table")
+				self.create_table_widget(rowPosition, 5, str(y.ptp), "net_addresses_table")
+    
+		for x in psutil.net_connections():
+			z = psutil.net_connections()
+			rowPosition = self.ui.net_connections_table.rowCount()
+			self.ui.net_connections_table.insertRow(rowPosition)
+
+			self.create_table_widget(rowPosition, 0, str(x.fd), "net_connections_table")
+			self.create_table_widget(rowPosition, 1, str(x.family), "net_connections_table")
+			self.create_table_widget(rowPosition, 2, str(x.type), "net_connections_table")
+			self.create_table_widget(rowPosition, 3,str(x.laddr), "net_connections_table")
+			self.create_table_widget(rowPosition, 4, str(x.raddr), "net_connections_table")
+			self.create_table_widget(rowPosition, 5, str(x.status), "net_connections_table")
+			self.create_table_widget(rowPosition, 5, str(x.pid), "net_connections_table")
+			
 # ## Execute App
 if __name__=="__main__":
 	app = QApplication(sys.argv)
